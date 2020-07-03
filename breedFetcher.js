d95b69acf8c055765d6480de0e0674a7819340eb
Angel-Ho221 <angel.ho@ryerson.ca>
2020-07-03 02:39:54 +0000

    refractored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,22 @@
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  // const data = body;
-  console.log(data);
-  console.log(typeof data);
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  if (response && response.statusCode >= 400) {
-    console.log(data.message);
-  } else if (data.length === 0) {
-    console.log("The breed you have searched does not exist.");
-  } else if (data.length !== 0) {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+
+    if (error) {
+      callback(error, null);
+    } else if (response && response.statusCode >= 400) {
+      let errMsg = data.message;
+      callback(errMsg, null);
+    } else if (data.length === 0) {
+      let errMsg = "The breed you have searched does not exist.";
+      callback(errMsg, null);
+    } else if (data.length !== 0) {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
